627a9ceb04785451e5aeea58d676b11d64566a95
kaj <kajdabrowski@gmail.com>
2021-05-12 14:44:52 +0200

    fixed createMessage

--- Controllers/messageController.js
+++ Controllers/messageController.js
@@ -1,22 +1,36 @@
 const { InvalidBody, UserNotFound } = require("../Errors");
+// const Task = require("../Models/taskModel")
 const Message = require("../Models/messageModel");
+const Task = require("../Models/taskModel");
 
 module.exports = {
   async createMessage(req, res, next) {
     try {
       const UserID = req.user.id;
-      const taskID = req.user.id;
+      const taskID = req.params.id;//FEL 
+      const task = await Task.findOne({where: {id:taskID}})
 
       const { title, content } = req.body;
       if (!title || !content) {
         throw new InvalidBody(["title", "content"]);
       }
       await Message.create({ title, content, UserID, taskID });
-      res.json({ message: "Message registered" });
+      res.json({ message: "Message created" });
     } catch (error) {
       next(error);
     }
   },
+
+// async createMessage(req, res, next) {
+//   try{
+//     const UserId = req.user.id
+//     const 
+
+//   }catch(error) {
+//     next(error);
+//   }
+
+// },
 
   async deleteMessageById(req, res, next) {
     try{
