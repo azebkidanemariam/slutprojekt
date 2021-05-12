2ac9f285409228918d1e4f775df23569e46a93cd
kaj <kajdabrowski@gmail.com>
2021-05-12 17:07:45 +0200

    Fixed foreignKey in message table, fixed createMessage in messageController

--- Controllers/messageController.js
+++ Controllers/messageController.js
@@ -2,19 +2,21 @@ const { InvalidBody, UserNotFound } = require("../Errors");
 // const Task = require("../Models/taskModel")
 const Message = require("../Models/messageModel");
 const Task = require("../Models/taskModel");
+const User = require("../Models/userModel");
 
 module.exports = {
   async createMessage(req, res, next) {
     try {
-      const UserID = req.user.id;
-      const taskID = req.params.id; //FEL
-      const task = await Task.findOne({ where: { id: taskID } });
+      const authorID = req.user.id;
+      const taskID = req.params.id;
+      const task = await Task.findOne({ where: { id: authorID } });
 
       const { title, content } = req.body;
       if (!title || !content) {
         throw new InvalidBody(["title", "content"]);
       }
-      await Message.create({ title, content, UserID, taskID });
+      const user = await User.findOne({where: {id:authorID}})
+      await Message.create({ title, content, authorID, taskID });
       res.json({ message: "Message created" });
     } catch (error) {
       next(error);
